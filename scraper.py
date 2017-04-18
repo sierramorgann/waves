8d92c7a4d654f6be69db10a0c9f24093596bb373
Sierra Morgan <you@example.com>
2017-04-18 10:14:00 -0400

    updates

--- scraper.py
+++ scraper.py
@@ -1,32 +1,48 @@
 #!/usr/bin/python
-import socket
+import serial
 import re
-from bs4 import BeautifulSoup
-import requests
 import json
 import csv
 import datetime
+import urllib
+import OSC
+from OSC import OSCClient, OSCMessage
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 6258 
-BUFFER_SIZE = 1024
+#umpqua off shore bouey wave height in feet and the period of the wave in seconds
 
-var = requests.get('http://www.ndbc.noaa.gov/data/realtime2/46229.data_spec').content
-another = BeautifulSoup(var, "html.parser")
+url = 'http://www.ndbc.noaa.gov/data/latest_obs/46229.rss'
+source = urllib.urlopen(url)
+content = source.read()
+source.close()
 
+waveHeight = re.findall(r'.\bWave Height\b.*', str(content))
+wavePeriod = re.findall(r'.\bWave Period\b.*', str(content))
+ft = re.findall(r'(?:\d*\.)?\d+', str(waveHeight))
+sec = re.findall(r'(?:\d*\.)?\d+', str(wavePeriod))
+feet = ''.join(ft)
+seconds = ''.join(sec)
+data = ','.join(ft + sec)
 
-for line in another:
-	p = re.sub(r'[\(]\d\.\d\d\d[\)]|[9]\.[9][9][9]|(?<![-.])\b[0-9]+\b(?!\.[0-9])', ",", line)
+ftData = float(feet)
+secData = int(float(seconds))
 
-# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# s.connect((TCP_IP, TCP_PORT))
-# s.send(p)
-# data = s.recv(BUFFER_SIZE)
-# s.close()		
+print(ftData)
+print(secData)
+
+	
 
 def timeStamped(fname, fmt='%Y-%m-%d-%H-%M-%S_{fname}'):
     return datetime.datetime.now().strftime(fmt).format(fname=fname)	
 
+    # /Users/sierramorgan/Desktop/waves/
+
 with open('/Users/sierramorgan/Desktop/waves/waves_data.csv','w') as data_out:
     writer=csv.writer(data_out)    
-    data_out.write(p)
+    data_out.write(data)
+
+# client = OSCClient()
+# client.connect( ('10.0.1.3', 4380) )
+# msg = OSCMessage("/send")
+# msg.append(ftData)
+# msg.append(secData)
+# client.send(msg)
